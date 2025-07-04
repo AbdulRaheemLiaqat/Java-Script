a77e0155e8d51d8b967abf5222b0be9d667f55ad
Abdul Raheem Liaqat <abdul.rahim.liaqat@gmail.com>
2025-07-04 11:47:26 +0500

    Add files via upload

---
+++ DAY _  2/logical-operator.js
@@ -0,0 +1,8 @@
+let speaksEnglish = true;
+let population = 40; 
+let isIsland = false;
+if (speaksEnglish && population < 50 && !isIsland) {
+  console.log("You should live in Portugal :)");
+} else {
+  console.log("Portugal does not meet your criteria :(");
+}
