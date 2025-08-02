800c88b7247ec9071d26a42c97a94848c6b797e4
Abdul Raheem Liaqat <abdul.rahim.liaqat@gmail.com>
2025-08-02 16:47:50 +0500

    Delete Java-script/function/Default Parameter directory

--- Java-script/function/Default Parameter/basic.js
+++
@@ -1,5 +0,0 @@
-function greet(name = 'Guest') {
-  console.log(`Hello, ${name}!`);
-}
-greet();          // Hello, Guest!
-greet('Abdullah');     // Hello, Abdullah!
--- Java-script/function/Default Parameter/multiple.js
+++
@@ -1,6 +0,0 @@
-function createUser(name = 'Unknown', age = 18) {
-  console.log(`Name: ${name}, Age: ${age}`);
-}
-createUser();               // Name: Unknown, Age: 18
-createUser('maha');         // Name: maha, Age: 18
-createUser('hassan', 25);    // Name: hassan, Age: 25
