8693327bd94ccfe20d9fe967568e4b6c6297864b
Abdul Raheem Liaqat <abdul.rahim.liaqat@gmail.com>
2025-08-16 20:46:46 +0500

    Add files via upload

---
+++ Java-script/Numbers ,Dates, INTL and Timers/Numbers/Converting and checking.js
@@ -0,0 +1,4 @@
+console.log(Number('23'));       // 23
+console.log(+'23');              // 23
+console.log(Number.isFinite(23));// true
+console.log(Number.isFinite('23'));// false
---
+++ Java-script/Numbers ,Dates, INTL and Timers/Numbers/Math and Rounding.js
@@ -0,0 +1,5 @@
+console.log(Math.sqrt(25));     // 5
+console.log(Math.max(5, 18, 23)); // 23
+console.log(Math.round(23.3));  // 23
+console.log(Math.ceil(23.3));   // 24
+console.log(Math.floor(23.9));  // 23
