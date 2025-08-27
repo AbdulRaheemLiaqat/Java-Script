a45637dd852b3d71654eb01cab70af06ce4bee03
Abdul Raheem Liaqat <abdul.rahim.liaqat@gmail.com>
2025-08-27 20:36:56 +0500

    Encapsulation Protected.js

---
+++ Java-script/OOP/Encapsulation Protected.js
@@ -0,0 +1,16 @@
+class BankAccount {
+  constructor(owner, balance) {
+    this.owner = owner
+    this._balance = balance
+  }
+  deposit(amount) {
+    this._balance += amount
+    return this._balance
+  }
+  getBalance() {
+    return this._balance
+  }
+}
+const acc = new BankAccount("Sara", 1000)
+acc.deposit(500)
+console.log(acc.getBalance())
