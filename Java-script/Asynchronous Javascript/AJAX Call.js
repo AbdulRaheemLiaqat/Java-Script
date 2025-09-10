87c8372310f9dc393280e73bfd6005a65ebb4849
Abdul Raheem Liaqat <abdul.rahim.liaqat@gmail.com>
2025-09-10 18:50:34 +0500

    AJAX Call.js

---
+++ Java-script/Asynchronous Javascript/AJAX Call.js
@@ -0,0 +1,7 @@
+const request = new XMLHttpRequest();
+request.open('GET', 'https://restcountries.com/v3.1/name/pakistan');
+request.send();
+request.addEventListener('load', function () {
+  const data = JSON.parse(this.responseText);
+  console.log(data);
+});
